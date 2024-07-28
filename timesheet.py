abe79ea570f8c9a88cd99b5de80fc7a261b100f7
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2024-07-28 17:22:18 -0400

    feat: automate login for hr self serve

--- timesheet.py
+++ timesheet.py
@@ -10,15 +10,36 @@ service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 driver.get("https://hrselfserve.info.yorku.ca/")
 
+username = input("") #Enter your ppy Username
+password = "XXXXXXX" #Enter your ppy password. 
+
 try:
     button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "linkser"))
     )
     button.click()
+    
+    username_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "mli"))
+    )
+
+    password_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "password"))
+    )
+
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input.btn.btn-lg.btn-primary"))
+    )
+    login_button.click()
+
+    
 except Exception as e:
     print(f"An error took place: {e}")
 
-time.sleep(10)
+time.sleep(20)
 
 driver.quit()
 
