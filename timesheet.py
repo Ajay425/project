48c8d48b984ca8fb37eaa1feb0844c079a1e8e7c
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2024-08-04 22:17:58 -0400

    TODO: fix input for out time
    Need to fix input for out time

--- timesheet.py
+++ timesheet.py
@@ -9,8 +9,11 @@ service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 driver.get("https://hrselfserve.info.yorku.ca/")
 
-username = "XXXXXX"  # Enter your ppy Username
-password = "XXXXXX"  # Enter your ppy password.
+username = ""  # Enter your ppy Username
+password = ""  # Enter your ppy password.
+timein = ""  #enter timein
+timeout ="" #enter timeout
+
 
 try:
     # Wait for the 'linkser' button to be clickable and click it
@@ -56,11 +59,31 @@ try:
     hours_card.click()
     print("Clicked the hours card button.")
 
+    enter_time = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@id='win0groupletPTNUI_LAND_REC_GROUPLET$1']"))
+    )
+    enter_time.click()
+    print("Clicked the enter time button.")
+
+    timein_field = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "PUNCH_TIME_1$8"))
+    )
+    timeout_field = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "PUNCH_TIME_4$8"))
+    )
+    
+    timein_field.send_keys(timein)
+    timeout_field.send_keys(timeout)
+    
+    #need to get the Id for in and out timestamps
+    #in timestamps = PUNCH_TIME_1$8
+    #out timestamp = PUNCH_TIME_4$8
+    #id for submit button TL_LINK_WRK_SUBMIT_PB
 except Exception as e:
     print(f"An error took place: {e}")
 
 # Wait for some time to observe the actions
-time.sleep(50)
+time.sleep(30)
 
 # Close the browser
 driver.quit()
